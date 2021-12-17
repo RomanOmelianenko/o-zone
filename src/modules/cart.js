13889ba07eaa2ee9dd7135d6fa2f7d283c0ac7ba
RomanOmelianenko <r_omelyanenko@ukr.net>
2021-12-17 20:04:03 +0200

    cart-modal

---
+++ src/modules/cart.js
@@ -0,0 +1,22 @@
+const cart = () => {
+  const cartBtn = document.getElementById("cart");
+  // console.dir(cart);
+  const cartModal = document.querySelector(".cart");
+  const cartCloseBtn = cartModal.querySelector(".cart-close");
+
+  const openCart = () => {
+    cartModal.style.display = "flex";
+  };
+
+  const closeCart = (e) => {
+    e.key === "Escape"
+      ? (cartModal.style.display = "")
+      : (cartModal.style.display = "");
+  };
+
+  cartBtn.addEventListener("click", openCart);
+  cartCloseBtn.addEventListener("click", closeCart);
+  window.addEventListener("keydown", closeCart);
+};
+
+export default cart;
